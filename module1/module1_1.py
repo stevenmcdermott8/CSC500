f550ba5cbaab23ba611be064c18995f853c2ab5b
Steven McDermott <stevenmcdermott8@gmail.com>
2025-09-05 22:36:35 -0700

    CSC500 module 1 thinking assignment

---
+++ module1/module1_1.py
@@ -0,0 +1,10 @@
+def main():
+        num1 = int(input('Enter a number: '))
+        num2 = int(input('Enter another number: '))
+        sumOfNumbers = num1 + num2
+        differenceOfNumbers = num1 - num2
+        print(num1, '+', num2, '=', sumOfNumbers)
+        print(num1, '-', num2, '=', differenceOfNumbers)
+
+if __name__ == "__main__":
+        main()
---
+++ module1/module1_2.py
@@ -0,0 +1,10 @@
+def main():
+        num1 = int(input('Enter a number: '))
+        num2 = int(input('Enter another number: '))
+        multiplicationOfNumbers = num1 * num2
+        divisionOfNumbers = num1 / num2
+        print(num1, '*', num2, '=', multiplicationOfNumbers)
+        print(num1, '/', num2, '=', divisionOfNumbers)
+
+if __name__ == "__main__":
+        main()
